c0b701ec384ef616d9d4a3bb85cfdc3011271fa6
Bridget <bridgetoconnor808@gmail.com>
2019-10-07 16:33:24 +0900

    created a illustration grid on the illustration page, the size does not scale with the page

--- src/components/illustration.tsx
+++ src/components/illustration.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { Component } from 'react'
+import IllustrationGrid from './IllustrationGrid'
 
 export default class Illustration extends Component {
   render() {
     return(
       <div>
         <h2> Illustration </h2>
-        <p> works created in Photoshop and Illustrator </p>
+        <p> Works created in Photoshop and other media </p>
+        <IllustrationGrid/>
       </div>
     )
   }
---
+++ src/components/tileData.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import Athens from './pictures/athens.jpg'
+import Bikes from './pictures/bikes.png'
+import Cranes from './pictures/cranes.png'
+import Forest from './pictures/forest.png'
+import Frog from './pictures/frog.jpg'
+import Lifedrawing from './pictures/lifeDrawings.jpg'
+import Painting from './pictures/painting.jpg'
+import Penart from './pictures/penart.png'
+import Racoon from './pictures/racoon.jpeg'
+
+export const tileData = [
+{
+  img: Painting,
+  title: "Kaunaoa",
+  materials: "Acrylic"
+},
+{
+  img: Bikes,
+  title: "Bike Pencilcase Design",
+  materials: "Photoshop"
+},
+{
+  img: Cranes,
+  title: "Crane Pattern",
+  materials: "Photoshop"
+},
+{
+  img: Forest,
+  title: "Background from A Great Chef",
+  materials: "Photoshop"
+},
+{
+  img: Frog,
+  title: "Frog Pencilcase Desgin",
+  materials: "Photoshop"
+},
+{
+  img: Lifedrawing,
+  title: "Life drawings",
+  materials: "Pen, Copic Markers, and Graphite"
+},
+{
+  img: Penart,
+  title: "Beyond the Path",
+  materials: "Pen"
+},
+{
+  img: Racoon,
+  title: "Racoon Pencilcase Design",
+  materials: "Photoshop"
+},
+{
+  img: Athens,
+  title: "Little Witch",
+  materials: "Photoshop and Graphite"
+},
+];
+
+export default {
+  tileData,
+}
