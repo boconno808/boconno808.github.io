a0fe0056a1b3358c9d0e126bcafd6719eb7c4998
Bridget <bridgetoconnor808@gmail.com>
2019-10-11 16:57:52 +0900

    added stepper

--- src/components/animTileData.tsx
+++ src/components/animTileData.tsx
@@ -1,12 +1,43 @@
 import * as React from 'react'
-import Athens from './pictures/eel.png'
+import Eel from './pictures/eel.png';
+import Chef from './pictures/greatchef.png';
+import Fight from './pictures/fight.png';
+import Game from './pictures/game.png';
+import Swat from './pictures/swat.png';
+import Wakeup from './pictures/WakeUpGirl.png';
+
 
 
 export const animTileData = [
   {
-    img: Eel,
-    title: "Kaunaoa",
-    materials: "Acrylic"
+    imgPath: Chef,
+    label: "A Great Chef",
+    materials: "Maya and Photoshop"
+  },
+  {
+    imgPath: Eel,
+    label: "The Snack",
+    materials: "Maya and Photoshop"
+  },
+  {
+    imgPath: Fight,
+    label: "Girl Vs Centipede",
+    materials: "Maya and Photoshop"
+  },
+  {
+    imgPath: Game,
+    label: "Mini Game",
+    materials: "Maya and Photoshop"
+  },
+  {
+    imgPath: Swat,
+    label: "Swat",
+    materials: "Maya and Photoshop"
+  },
+  {
+    imgPath: Wakeup,
+    label: "Wakeup",
+    materials: "Maya and Photoshop"
   },
 ]
 
