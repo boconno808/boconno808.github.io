a0300bc2bfe2e68f184cf1e34ffb39de0d420ba1
Bridget <bridgetoconnor808@gmail.com>
2021-04-05 16:50:00 -1000

    fixing the bio and contact

--- src/components/about/aboutCard.tsx
+++ src/components/about/aboutCard.tsx
@@ -1,35 +1,36 @@
-import * as React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
+import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     padding: {
       flexGrow: 1,
       padding: theme.spacing(6),
-      margin: 'auto',
+      margin: "auto",
     },
     card: {
       maxWidth: 600,
-      margin: 'auto',
+      margin: "auto",
     },
     face: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
     },
-    paragraph:{
+    paragraph: {
       paddingTop: theme.spacing(2),
     },
     divider: {
       maxWidth: 100,
-      margin:'auto',
+      margin: "auto",
     },
-  }),
+  })
 );
 
 export default function AboutCard() {
@@ -37,27 +38,29 @@ export default function AboutCard() {
 
   return (
     <div className={classes.padding}>
-    <Grid className={classes.face}>
-      <img src={require('../pictures/linkedin.png')} alt='selfPortrait' />
-    </Grid>
+      <Grid className={classes.face}>
+        <img src={require("../pictures/linkedin.png")} alt="selfPortrait" />
+      </Grid>
       <Card className={classes.card}>
         <CardContent>
-          <Typography variant="h5">
-            About
-          </Typography>
+          <Typography variant="h5">About</Typography>
           <Divider className={classes.divider} />
-          <Typography align='center' variant="body1" paragraph className={classes.paragraph}>
-          Bridget O’Connor is an Animation and Computer Science student at Loyola Marymount University
-          who is passionate about eating. She gained an interest in CS through her love for computers and
-          puzzling things out, and also enjoys art as well. By doing a double degree she
-           hopes to pursue a career combining those two but more focused in CS. However, her passion for snacking is a pursuit she
-            plans to follow with or without a degree.
+          <Typography
+            align="center"
+            variant="body1"
+            paragraph
+            className={classes.paragraph}
+          >
+            Bridget O’Connor is an Animation and Computer Science student at
+            Loyola Marymount University. She gained an interest in CS through
+            her love for computers and puzzling things out, and also enjoys art
+            as well. By doing a double degree she hopes to pursue a career
+            combining those two but more focused in CS.
           </Typography>
-          <Typography align='center' variant="body1" gutterBottom>
-          Half Japanese⎯she was born and raised in Honolulu, Hawai'i and is also minoring in Asian and
-           Pacific studies. She creates work that she hopes people can find interesting or useful. She has
-            experience designing and creating pouches and bags, all of which can be used to hold various
-             amounts of snacks.
+          <Typography align="center" variant="body1" gutterBottom>
+            She creates work that she hopes people can find interesting or
+            useful. She has experience designing and creating pouches and bags,
+            all of which can be used to hold various amounts of snacks.
           </Typography>
         </CardContent>
       </Card>
--- src/components/contactCard.tsx
+++ src/components/contactCard.tsx
@@ -1,74 +1,77 @@
-import * as React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
+import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     padding: {
       flexGrow: 1,
       padding: theme.spacing(6),
-      margin: 'auto',
+      margin: "auto",
     },
     card: {
       maxWidth: 600,
-      margin: 'auto',
+      margin: "auto",
     },
     icons: {
-      justifyContent: 'center'
+      justifyContent: "center",
     },
     mailbox: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
     },
     divider: {
       maxWidth: 100,
-      margin:'auto',
+      margin: "auto",
     },
     paddingPhone: {
       paddingTop: 20,
-    }
-  }),
+    },
+  })
 );
 
 export default function ContactCard() {
   const classes = useStyles({});
 
   return (
-      <div className={classes.padding}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Grid className={classes.mailbox}>
-            <img src={require('./pictures/mailbox.png')} alt='mailbox'/>
-            </Grid>
-            <Typography variant="h5">
-              Contact
-            </Typography>
-            <Divider className={classes.divider} />
-            <Typography variant="subtitle1" gutterBottom>
-              I'd love to talk with you about anything!
-            </Typography>
-            <Typography variant="body2" component="p" className={classes.paddingPhone}>
-              boconno9@lion.lmu.edu
-            </Typography>
-          </CardContent>
-          <CardActions className={classes.icons}>
-            <IconButton aria-label={`linkedIn`} target="_blank" href="https://www.linkedin.com/in/bridgetoco/">
-              <LinkedInIcon />
-            </IconButton>
-            <IconButton aria-label={`instagram`} target="_blank" href="https://www.instagram.com/bridgetoconno/">
-              <InstagramIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
-      </div>
+    <div className={classes.padding}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Grid className={classes.mailbox}>
+            <img src={require("./pictures/mailbox.png")} alt="mailbox" />
+          </Grid>
+          <Typography variant="h5">Contact</Typography>
+          <Divider className={classes.divider} />
+          <Typography variant="subtitle1" gutterBottom>
+            I'd love to talk with you about anything!
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p"
+            className={classes.paddingPhone}
+          >
+            boconno9@lion.lmu.edu
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.icons}>
+          <IconButton
+            aria-label={`linkedIn`}
+            target="_blank"
+            href="https://www.linkedin.com/in/bridgetoco/"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
